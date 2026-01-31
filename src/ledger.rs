5bc64c07bd0dfa4068e3fcc9e721f149dd325303
Bianca Correa <me@aquabianca.dev>
2026-01-31 16:16:38 -0300

    tests for ledger

--- src/ledger.rs
+++ src/ledger.rs
@@ -1,1 +1,19 @@
 pub mod database;
+
+use serde::{Deserialize, Serialize};
+
+use crate::database::Database;
+
+#[cfg(test)]
+pub mod unit;
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Ledger {
+    pub database: Database,
+}
+
+impl Ledger {
+    pub fn new(database: Database) -> Self {
+        Ledger { database: database }
+    }
+}
---
+++ src/ledger/unit.rs
@@ -0,0 +1,12 @@
+mod test {
+    use crate::database;
+    use crate::ledger;
+
+    fn new() {
+        let db_name = "my-database.duckdb";
+        let db = database::Database::new(db_name);
+
+        let l = ledger::Ledger::new(db);
+        assert_eq!(l.database.name, db_name)
+    }
+}
