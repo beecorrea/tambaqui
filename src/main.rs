b59552621680eb18f2b0860eb279a395f5cec81c
Bianca Correa <me@aquabianca.dev>
2026-01-31 19:25:27 -0300

    read ledger from yaml file

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 mod ledger;
 
 use ledger::database;
+use std::error::Error;
+use std::fs;
 
-fn main() {
-    let serialized = "
-database:
-    name: my-database
-";
-
-    let l = ledger::Ledger::new(&serialized);
+fn main() -> Result<(), Box<dyn Error>> {
+    let ledger_config = fs::read_to_string("ledger.example.yaml")?;
+    let l = ledger::Ledger::new(&ledger_config);
     println!("Using database {}", l.database.name);
+
+    Ok(())
 }
